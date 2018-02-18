c4a441709f851fa56b235dc73e5179f5619aaa40
Nathan Ross Powell <nathanrosspowell@gmail.com>
2018-02-18 17:54:46 -0500

    Basic templates for HOME

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,12 @@ module.exports = function(grunt) {
                         expand: true, 
                     },
                     {
+                        cwd: 'assets/',
+                        src: ['**/*'],
+                        dest: '_temp/',
+                        expand: true, 
+                    },
+                    {
                         cwd: '_temp/build/',
                         src: ['**/*'],
                         dest: '_build/',
@@ -20,18 +26,21 @@ module.exports = function(grunt) {
             }
         },
         'compile-handlebars': {
-            programmer_talk: {
+            home: {
                 template: [
-                    'src/child_page/template.handlebars',
-                    'src/remark/header.handlebars',
+                    'templates/header.html',
+                    'templates/home.html',
+                    'templates/footer.html'
                 ],
                 templateData: [
-                    'src/beginner_guides/programmer_talk/meta.json',
-                    'src/beginner_guides/programmer_talk/meta.json',
+                    'data/pages/home.json',
+                    'data/pages/home.json',
+                    'data/pages/home.json'
                 ],
                 output: [
-                    'build/beginner_guides/programmer_talk/index.html',
-                    'temp/beginner_guides/programmer_talk/presentation/index.html',
+                    '_temp/home/header.html',
+                    '_temp/home/index.html',
+                    '_temp/home/footer.html'
                 ]
             }
         },
@@ -39,10 +48,11 @@ module.exports = function(grunt) {
             main: {
                 files: {
                     // programmer_talk
-                    'build/beginner_guides/programmer_talk/presentation/index.html' : [
-                        'temp/beginner_guides/programmer_talk/presentation/index.html',
-                        'src/beginner_guides/programmer_talk/README.md',
-                        'src/remark/footer.html' ]
+                    '_build/index.html' : [
+                        '_temp/home/header.html',
+                        '_temp/home/index.html',
+                        '_temp/home/footer.html'
+                    ]
                 }
             }
         },
@@ -61,7 +71,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-compile-handlebars');
     grunt.loadNpmTasks('grunt-gh-pages');
     // Default task: does everything except deployment
-    grunt.registerTask('default', ['copy', ]); //'compile-handlebars', 'concat' ]);
+    grunt.registerTask('default', ['copy', 'compile-handlebars', 'concat' ]);
     // Deploy task: runs everything in order.
     grunt.registerTask('deploy', ['default', 'gh-pages']);
 };
